95195b5aa61e462e34cc53a9ecafb376bd485243
ShubhiSaxena12 <shubhi.saxena@jabong.com>
2017-06-07 16:38:10 +0530

    Add files via upload

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+module.exports={
+	entry: './client.js',
+	output: {
+		filename: 'bundle.js'
+	},
+	module: {
+		loaders:[
+		{
+			test: /\.js$/,
+			exclude: /node_modules/,
+			loader: 'babel-loader'
+		}
+		]
+	},
+	resolve:{
+		extensions : ['','.js','.json']
+
+	}
+};
